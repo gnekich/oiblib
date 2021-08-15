7083caf46e774b7b6ffe8dbbf2968470e69dc610
Gordan Nekić <gordan@neki.ch>
2021-08-15 15:37:14 +0200

    (update) Update dependencies and replace legacy mocha assert method .equal with .strictEqual

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,24 +6,24 @@ const assert = require('assert');
 describe('Validate OIB and JMBG', function() {
 
   it('OIB', function(done) {
-    assert.equal(validateOIB(3), false);
-    assert.equal(validateOIB('00000000001'), true);
-    assert.equal(validateOIB('00000000003'), false);
+    assert.strictEqual(validateOIB(3), false);
+    assert.strictEqual(validateOIB('00000000001'), true);
+    assert.strictEqual(validateOIB('00000000003'), false);
     done();
   })
 
   it('JMBG', function(done) {
-    assert.equal(validateJMBG(3), false);
-    assert.equal(validateJMBG('0000000000000'), true);
-    assert.equal(validateJMBG('0000000000001'), false);
-    assert.equal(validateJMBG('0000000000002'), false);
-    assert.equal(validateJMBG('0000000000003'), false);
-    assert.equal(validateJMBG('0000000000004'), false);
-    assert.equal(validateJMBG('0000000000005'), false);
-    assert.equal(validateJMBG('0000000000006'), false);
-    assert.equal(validateJMBG('0000000000007'), false);
-    assert.equal(validateJMBG('0000000000008'), false);
-    assert.equal(validateJMBG('0000000000009'), false);
+    assert.strictEqual(validateJMBG(3), false);
+    assert.strictEqual(validateJMBG('0000000000000'), true);
+    assert.strictEqual(validateJMBG('0000000000001'), false);
+    assert.strictEqual(validateJMBG('0000000000002'), false);
+    assert.strictEqual(validateJMBG('0000000000003'), false);
+    assert.strictEqual(validateJMBG('0000000000004'), false);
+    assert.strictEqual(validateJMBG('0000000000005'), false);
+    assert.strictEqual(validateJMBG('0000000000006'), false);
+    assert.strictEqual(validateJMBG('0000000000007'), false);
+    assert.strictEqual(validateJMBG('0000000000008'), false);
+    assert.strictEqual(validateJMBG('0000000000009'), false);
     done();
   })
 
@@ -34,14 +34,14 @@ describe('Generate random valid OIB and JMBG', function() {
   it('Generate OIB', function(done) {
     const newOIB = generatePossibleOIB();
     // console.log(`Generated OIB: ${newOIB}`, validateOIB(newOIB));
-    assert.equal(validateOIB(newOIB), true);
+    assert.strictEqual(validateOIB(newOIB), true);
     done();
   })
 
   it('JMBG', function(done) {
     const newJMBG = generatePossibleJMBG();
     // console.log(`Generated JMBG: ${newJMBG}`, validateJMBG(newJMBG));
-    assert.equal(validateJMBG(newJMBG), true);
+    assert.strictEqual(validateJMBG(newJMBG), true);
     done();
   })
 
